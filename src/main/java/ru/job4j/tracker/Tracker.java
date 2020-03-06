3b86c5f3de7001bbddaee307244ef5f89cea1c42
Zelenko Alexandr <zely20@mail.ru>
2020-03-06 10:03:54 +0300

    add Tracker.java and Item.java

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,33 @@
 package ru.job4j.tracker;
 
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        //Реализовать метод генерации.
+        return null;
+    }
 }
