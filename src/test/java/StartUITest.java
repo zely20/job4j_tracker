c076a39c4d53676127ce8a3e40a7ed3042dc7919
Zelenko Alexandr <zely20@mail.ru>
2020-03-22 18:25:08 +0300

    9. Написать тесты на StartUI.[#240161]

--- src/test/java/StartUITest.java
+++ src/test/java/StartUITest.java
@@ -1,47 +1,17 @@
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.tracker.*;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
-@Ignore
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        //StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
-
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(), // id сохраненной заявки в объект tracker.
-                "replaced item"
-        };
-       // StartUI.editItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId()
-        };
-        //StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertNull(deleted);
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"1"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action, action});
+        assertThat(action.isCall(), is(true));
     }
 }
