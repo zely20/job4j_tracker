cdf9c6d5ddd2ce849ef5b63bd00576936bcae74e
Zelenko Alexandr <zely20@mail.ru>
2020-04-06 20:05:03 +0300

    1. Обеспечить бесперебойную работу приложения Tracker.[#240174]

--- src/main/java/ru/job4j/tracker/Input.java
+++ src/main/java/ru/job4j/tracker/Input.java
@@ -5,4 +5,6 @@ public interface Input {
     String askStr(String question);
 
     int askInt(String question);
+
+    int askInt(String question, int max);
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = input.askInt("Select: ");
+            int select = input.askInt("Select: ",6);
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -20,7 +20,7 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(),
