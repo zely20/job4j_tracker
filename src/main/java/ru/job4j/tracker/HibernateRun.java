1c12109f8abeaff55acbec73bc70590af0fe0848
zely20@mail.ru <Civik1984>
2021-06-07 21:35:34 +0300

    ref

--- src/main/java/ru/job4j/tracker/HibernateRun.java
+++ src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.sql.Timestamp;
 import java.util.List;
 
     public class HibernateRun {
@@ -13,14 +15,14 @@ import java.util.List;
                     .configure().build();
             try {
                 SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-                Item item = create(new Item("Learn Hibernate"), sf);
+                Item item = create(new Item("Learn Hibernate", new Timestamp(System.currentTimeMillis()), "dfsdfsdsd"), sf);
                 System.out.println(item);
                 item.setName("Learn Hibernate 5.");
                 update(item, sf);
                 System.out.println(item);
                 Item rsl = findById(item.getId(), sf);
                 System.out.println(rsl);
-                delete(rsl.getId(), sf);
+               // delete(rsl.getId(), sf);
                 List<Item> list = findAll(sf);
                 for (Item it : list) {
                     System.out.println(it);
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
@@ -11,6 +12,8 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String description;
+    private Timestamp created;
 
     public Item(String name) {
         this.name = name;
@@ -19,6 +22,35 @@ public class Item {
     public Item(Integer id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Item(String name, Timestamp created, String description) {
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public Item(Integer id, String name, String description, Timestamp created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
     }
 
     public Item() {
@@ -47,17 +79,23 @@ public class Item {
         if (o == null || getClass() != o.getClass()) return false;
         Item item = (Item) o;
         return Objects.equals(id, item.id) &&
-                Objects.equals(name, item.name);
+                Objects.equals(name, item.name) &&
+                Objects.equals(description, item.description) &&
+                Objects.equals(created, item.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(id, name, description, created);
     }
 
     @Override
     public String toString() {
-        return "Item id is " + id  +
-                ", Item name is " + name ;
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                '}';
     }
 }
